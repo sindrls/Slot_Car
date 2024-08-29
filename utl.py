6ec116745226fc82c4d64170d996a63fa9346968
Håkon Vågsether <hakon.vagsether@gmail.com>
2024-08-29 12:00:29 +0200

    Add pickle helper functions

---
+++ utl.py
@@ -0,0 +1,13 @@
+# Slot car utilities
+
+import pickle
+
+
+def p_load(name: str):
+    with open(f'{name}.pkl', 'rb') as f:
+        return pickle.load(f)
+
+
+def p_save(element, name: str):
+    with open(f'{name}.pkl', 'wb') as f:
+        return pickle.dump(f)
