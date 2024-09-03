44720cfdfc3dc0cad3439ee49fa6646fd6de1c16
Håkon Vågsether <hakon.vagsether@gmail.com>
2024-09-03 10:17:33 +0200

    Even more cleanup

--- tyre_friction_modelling.py
+++ tyre_friction_modelling.py
@@ -24,6 +24,7 @@ def dynamic_regularized_friction_model(vel):
     return regular_slide_frict_coeff * np.sin(
         regular_sin_scaling_coeff * np.arctan((1 / (np.pi * regular_slide_speed_tresh)) * vel))
 
+
 def lift_Cd(angle):
     return np.sin()
 
@@ -33,8 +34,6 @@ for vel in test_velocities:
     atan_friction[idx] = dynamic_atan_friction_model(vel)
     regular_friction[idx] = lift_Cd(vel)
     idx = idx + 1
-
-
 
 #plt.plot(test_velocities, atan_friction, 'b', label='atan friction')
 
